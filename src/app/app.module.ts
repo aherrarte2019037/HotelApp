d1918862e45edccca318946ef3007414e204c337
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2021-04-17 13:42:06 -0600

    Se editó estilo sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { SidebarModule } from 'ng-sidebar';
@@ -21,6 +21,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component'
     SidebarModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
